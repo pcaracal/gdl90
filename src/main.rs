cbf114167fd940584509af8694df4a7c4c8d5215
pcaracal <peter.schreivogel@stud.kbw.ch>
2024-12-09 20:50:41 +0100

    create traffic report decoder

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 #[macro_use]
 extern crate log;
-use gdl90::messages::traffic_report::TrafficAlertStatus;
+use gdl90::messages::traffic_report::TrafficReport;
 use tokio::net::UdpSocket;
 
 #[tokio::main]
@@ -17,8 +17,14 @@ async fn main() -> anyhow::Result<()> {
         let (len, addr) = socket.recv_from(&mut buf).await?;
         let buf = &buf[..len];
         info!("Received {} bytes from {}", buf.len(), addr);
-        debug!("Data: {:?}", buf);
+
+        let tr = TrafficReport::from_bytes(&buf[2..]);
+        info!("{:#?}", tr);
     }
 
-    Ok(())
+    // 3.5.2 Traffic Report Example
+    // let data = &[
+    //     0x14, 0x00, 0xAB, 0x45, 0x49, 0x1F, 0xEF, 0x15, 0xA8, 0x89, 0x78, 0x0F, 0x09, 0xA9,
+    //     0x07, 0xB0, 0x01, 0x20, 0x01, 0x4E, 0x38, 0x32, 0x35, 0x56, 0x20, 0x20, 0x20, 0x00,
+    // ];
 }
--- src/messages/traffic_report.rs
+++ src/messages/traffic_report.rs
@@ -1,3 +1,6 @@
+use anyhow::anyhow;
+use log::debug;
+
 #[derive(Debug, Default, PartialEq, Eq)]
 pub enum TrafficAlertStatus {
     #[default]
@@ -80,11 +83,11 @@ impl From<u8> for MiscIndicators {
             3 => TrackHeading::HeadingTrue,
             _ => TrackHeading::Invalid,
         };
-        let report_type = match misc & 0b0000_0100 {
+        let report_type = match misc & 0b0000_0100 >> 2 {
             1 => ReportType::Extrapolated,
             _ => ReportType::Updated,
         };
-        let ground_state = match misc & 0b0000_1000 {
+        let ground_state = match misc & 0b0000_1000 >> 3 {
             1 => AirGround::Airborne,
             _ => AirGround::OnGround,
         };
@@ -109,10 +112,29 @@ pub enum NIC {
     NM0_6,
     NM0_2,
     NM0_1,
-    HPL75_VPL112,
-    HPL25_VPL37_5,
-    HPL7_5_VPL11,
+    HPL_75M_VPL_112M,
+    HPL_25M_VPL_37_5M,
+    HPL_7_5M_VPL_11M,
     Unused,
+}
+
+impl From<u8> for NIC {
+    fn from(nic: u8) -> Self {
+        match nic {
+            1 => Self::NM20,
+            2 => Self::NM8,
+            3 => Self::NM4,
+            4 => Self::NM2,
+            5 => Self::NM1,
+            6 => Self::NM0_6,
+            7 => Self::NM0_2,
+            8 => Self::NM0_1,
+            9 => Self::HPL_75M_VPL_112M,
+            10 => Self::HPL_25M_VPL_37_5M,
+            11 => Self::HPL_7_5M_VPL_11M,
+            _ => Self::Unknown,
+        }
+    }
 }
 
 #[derive(Debug, Default, PartialEq, Eq)]
@@ -128,10 +150,29 @@ pub enum NACp {
     NM0_3,
     NM0_1,
     NM0_05,
-    HFOM30_VFOM45,
-    HFOM10_VFOM15,
-    HFOM3_VFOM4,
+    HFOM_30M_VFOM_45M,
+    HFOM_10M_VFOM_15M,
+    HFOM_3M_VFOM_4M,
     Unused,
+}
+
+impl From<u8> for NACp {
+    fn from(nacp: u8) -> Self {
+        match nacp {
+            1 => Self::NM10,
+            2 => Self::NM4,
+            3 => Self::NM2,
+            4 => Self::NM1,
+            5 => Self::NM0_5,
+            6 => Self::NM0_3,
+            7 => Self::NM0_1,
+            8 => Self::NM0_05,
+            9 => Self::HFOM_30M_VFOM_45M,
+            10 => Self::HFOM_10M_VFOM_15M,
+            11 => Self::HFOM_3M_VFOM_4M,
+            _ => Self::Unknown,
+        }
+    }
 }
 
 #[derive(Debug, Default, PartialEq, Eq)]
@@ -214,16 +255,97 @@ pub struct TrafficReport {
     traffic_alert_status: TrafficAlertStatus,
     address_type: AddressType,
     participant_address: u32,
-    latitude_deg: f32,
-    longitude_deg: f32,
-    pressure_altitude_ft: i16,
+    latitude_deg: f64,
+    longitude_deg: f64,
+    pressure_altitude_ft: i32,
     misc_indicators: MiscIndicators,
     nic: NIC,
     nacp: NACp,
     horizontal_velocity_kt: u16,
-    vertical_velocity_fpm: f32,
-    track_heading: f32,
+    vertical_velocity_fps: f64,
+    track_heading: f64,
     emitter_category: EmitterCategory,
     call_sign: String,
     emergy_priority_code: EmergencyPriorityCode,
+}
+
+impl TrafficReport {
+    #[must_use]
+    pub fn new() -> Self {
+        Self::default()
+    }
+
+    /// Creates a `TrafficReport` from a byte buffer
+    /// buffer is expected to begin with `st` and end with `px` as per
+    /// <https://www.faa.gov/sites/faa.gov/files/air_traffic/technology/adsb/archival/GDL90_Public_ICD_RevA.PDF> 3.5.1
+    ///
+    /// # Errors
+    ///
+    /// Returns an error if the buffer is not 27 bytes long
+    pub fn from_bytes(buf: &[u8]) -> anyhow::Result<Self> {
+        if buf.len() < 27 {
+            return Err(anyhow!("Invalid buffer length"));
+        }
+
+        let mut tr = Self::new();
+
+        tr.traffic_alert_status = TrafficAlertStatus::from(buf[0] >> 4);
+        debug!("Traffic Alert Status: {:?}", tr.traffic_alert_status);
+        tr.address_type = AddressType::from(buf[0] & 0x0F);
+        debug!("Address Type: {:?}", tr.address_type);
+
+        tr.participant_address = u32::from_be_bytes(buf[1..5].try_into()?) >> 8;
+        debug!("Participant Address: {}", tr.participant_address);
+
+        let lat = i32::from_be_bytes(buf[4..8].try_into()?) >> 8;
+        tr.latitude_deg = f64::from(lat) / f64::from(0x7F_FFFF) * 180.0;
+        debug!("Latitude: {}", tr.latitude_deg);
+
+        let lon = i32::from_be_bytes(buf[7..11].try_into()?) / 256;
+        tr.longitude_deg = f64::from(lon) / f64::from(0x7F_FFFF) * 180.0;
+        debug!("Longitude: {}", tr.longitude_deg);
+
+        tr.pressure_altitude_ft =
+            i32::from(i16::from_be_bytes(buf[10..12].try_into()?) >> 4) * 25 - 1000;
+        debug!("Pressure Altitude: {}", tr.pressure_altitude_ft);
+        tr.misc_indicators = MiscIndicators::from(buf[11]);
+        debug!("Misc Indicators: {:?}", tr.misc_indicators);
+
+        tr.nic = NIC::from(buf[12] >> 4);
+        debug!("NIC: {:?}", tr.nic);
+        tr.nacp = NACp::from(buf[12] & 0b0000_1111);
+        debug!("NACp: {:?}", tr.nacp);
+
+        tr.horizontal_velocity_kt = u16::from_be_bytes(buf[13..15].try_into()?) >> 4;
+        debug!("Horizontal Velocity: {}", tr.horizontal_velocity_kt);
+
+        let mut vv1 = buf[14] & 0x0F;
+        let vv2 = buf[15];
+        if vv1 & 0b0000_1000 != 0 {
+            vv1 |= 0xF0;
+        }
+
+        tr.vertical_velocity_fps = f64::from(i16::from_be_bytes([vv1, vv2])) / 64.0 * 60.0;
+
+        debug!("Vertical Velocity: {}", tr.vertical_velocity_fps);
+
+        tr.track_heading = f64::from(buf[16]) / 256.0 * 360.0;
+        debug!("Track Heading: {}", tr.track_heading);
+
+        tr.emitter_category = EmitterCategory::from(buf[17]);
+        debug!("Emitter Category: {:?}", tr.emitter_category);
+
+        tr.call_sign = buf[18..26]
+            .iter()
+            .map(|&c| c as char)
+            .collect::<String>()
+            .trim()
+            .to_string();
+        debug!("Call Sign: {}", tr.call_sign);
+
+        tr.emergy_priority_code = EmergencyPriorityCode::from(buf[26] >> 4);
+        debug!("Emergency Priority Code: {:?}", tr.emergy_priority_code);
+
+        Ok(tr)
+    }
 }
