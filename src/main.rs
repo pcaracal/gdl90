17ec99d4b8ef017e9f135d1d0276c8af906433e5
pcaracal <peter.schreivogel@stud.kbw.ch>
2025-01-09 22:15:09 +0100

    new testing method to send movement

--- src/main.rs
+++ src/main.rs
@@ -1,51 +1,94 @@
 #[macro_use]
 extern crate log;
-use std::sync::Arc;
+use std::{env, sync::Arc, time::Duration};
 
-use gdl90::{crc_init, GDL90Message};
-use tokio::net::UdpSocket;
+use gdl90::{crc_init, messages::traffic_report::TrafficReportBuilder, GDL90Message};
+use tokio::{
+    net::UdpSocket,
+    time::{interval, Instant},
+};
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     dotenvy::dotenv().ok();
     pretty_env_logger::init();
 
-    let socket = UdpSocket::bind("127.0.0.1:4000").await?;
+    let socket = UdpSocket::bind("0.0.0.0:4000").await?;
     socket.set_broadcast(true)?;
     info!("UDP Listening on: {}", socket.local_addr()?);
 
     let crc_table = Arc::new(crc_init());
 
+    let mut tr = TrafficReportBuilder::new()
+        .with_lat_lon_alt_hdg(
+            50.033687266045376,
+            8.525938397372625,
+            1000,
+            179.0,
+            gdl90::messages::traffic_report::TrackHeading::HeadingTrue,
+        )
+        .horizontal_velocity_kt(40)
+        .build();
+
+    let mut i = interval(Duration::from_millis(100));
+    let mut ts = Instant::now();
     loop {
-        let mut buf = vec![0; 1024];
-        let (len, addr) = socket.recv_from(&mut buf).await?;
-        let buf = &buf[..len];
-        info!("Received {} bytes from {}", buf.len(), addr);
+        let msg = GDL90Message::OwnshipReport(tr.clone());
+        let bytes = msg.to_bytes(&crc_table);
 
-        // let bytes = buf
-        //     .iter()
-        //     .map(|b| format!("{b:02X}"))
-        //     .collect::<Vec<String>>()
-        //     .join(" ");
-        // debug!("Bytes: {}", bytes);
+        let r = socket
+            .send_to(
+                &bytes,
+                env::var("SEND_TO").expect("SEND_TO env var must be set"),
+            )
+            .await;
+        info!("Sent: {:?}", r);
 
-        let msg = GDL90Message::from_bytes(&crc_table, &buf[1..buf.len() - 1]);
+        let plat = tr.latitude_deg;
+        let plon = tr.longitude_deg;
+        let pspd = tr.horizontal_velocity_kt;
+        let phdg = tr.track_heading;
+        debug!(
+            "Ownship: lat={}, lon={}, spd={}, hdg={}",
+            plat, plon, pspd, phdg
+        );
 
-        let Ok(msg) = msg else { continue };
-        info!("Message: {:#?}", msg);
-        // let bytes = msg.to_bytes(&crc_table);
-        // let bytes = bytes
-        //     .iter()
-        //     .map(|b| format!("{b:02X}"))
-        //     .collect::<Vec<String>>()
-        //     .join(" ");
-        // debug!("Bytes: {}", bytes);
+        let (lat, lon) = move_pos(
+            (plat, plon),
+            pspd.into(),
+            phdg,
+            Instant::now().duration_since(ts).as_secs_f64(),
+        );
+
+        tr.latitude_deg = lat;
+        tr.longitude_deg = lon;
+
+        ts = Instant::now();
+        i.tick().await;
     }
+}
 
-    // 3.5.2 Traffic Report Example
-    // let data = &[
-    //     0x14, 0x00, 0xAB, 0x45, 0x49, 0x1F, 0xEF, 0x15, 0xA8, 0x89, 0x78, 0x0F, 0x09, 0xA9,
-    //     0x07, 0xB0, 0x01, 0x20, 0x01, 0x4E, 0x38, 0x32, 0x35, 0x56, 0x20, 0x20, 0x20, 0x00,
-    // ];
-    // Ok(())
+#[must_use]
+pub fn move_pos(
+    start: (f64, f64),
+    speed_knots: f64,
+    heading_deg: f64,
+    duration_secs: f64,
+) -> (f64, f64) {
+    const EARTH_RADIUS_NM: f64 = 3443.92;
+    let speed_nmps = speed_knots / 3600.0;
+    let distance_nm = speed_nmps * duration_secs;
+
+    let lat1 = start.0.to_radians();
+    let lon1 = start.1.to_radians();
+    let heading = heading_deg.to_radians();
+    let central = distance_nm / EARTH_RADIUS_NM;
+
+    let lat2 = (lat1.sin() * central.cos() + lat1.cos() * central.sin() * heading.cos()).asin();
+
+    let lon2 = (heading.sin() * central.sin() * lat1.cos())
+        .atan2(central.cos() - lat1.sin() * lat2.sin())
+        + lon1;
+
+    (lat2.to_degrees(), lon2.to_degrees())
 }
